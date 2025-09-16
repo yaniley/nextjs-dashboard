901c5fd5cbb9440d5f69f9652760b3149f2d85ae
Yaniley Rhobie C. Daroca <yanileydaroca@gmail.com>
2025-09-16 20:02:47 +0800

    added yaniley page

---
+++ app/yaniley/page.tsx
@@ -0,0 +1,8 @@
+import Info from "./info";
+export default function Page() {
+    return (
+        <div>
+            <h1 className="text=3x1 font-bold">Yaniley Rhobie C. Daroca</h1>
+        </div>
+    )
+}
