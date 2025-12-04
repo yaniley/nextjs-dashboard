348d377e6bebb0d4d34bdcc55ba9ac6cfa9bb56a
Yaniley Rhobie C. Daroca <yanileydaroca@gmail.com>
2025-12-04 18:11:31 +0800

    chapter 11 - adding mutating data

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,7 @@ import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
