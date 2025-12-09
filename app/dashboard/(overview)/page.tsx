25a8280daeaec10e3c3b974de79bf52d6c338412
Yaniley Rhobie C. Daroca <yanileydaroca@gmail.com>
2025-12-09 14:46:15 +0800

    added chapter 15 - adding metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,6 +9,11 @@ import {
   CardsSkeleton,
  } from '@/app/ui/skeletons';
  import CardWrapper from '@/app/ui/dashboard/cards';
+ import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard | Acme Dashboard',
+};
 
 export default async function Page() {
   return (
