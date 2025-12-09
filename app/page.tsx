c1c961d3f9f22701a0aebec57ac1813183f9d3d6
Yaniley Rhobie C. Daroca <yanileydaroca@gmail.com>
2025-12-09 18:13:33 +0800

    update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Page() {
             src="/hero-desktop.png"
             width={560}
             height={620}
-            className="hidden md:block"
+            className="hidden md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
